c68edf0031945b41c4a13dec785c45b4d6cb36ae
ecwu <bitlabwzh@gmail.com>
2018-05-12 01:56:48 +0800

    prepare for the ajax function

--- sourcecode/create/views.py
+++ sourcecode/create/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -16,7 +16,12 @@ def create_coach(request):
         return redirect('/')
     return render(request, 'create/coach.html')
 
+
 def result(request):
     if not request.user.is_authenticated:
         return redirect('/')
     return render(request, 'create/result.html')
+
+
+def user_name_check(request):
+    pass
