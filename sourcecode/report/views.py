a5493af525b8d564b75fbd97566f08ce4ad7861e
ecwu <bitlabwzh@gmail.com>
2018-05-15 11:31:34 +0800

    replace with some dynamic data

--- sourcecode/report/views.py
+++ sourcecode/report/views.py
@@ -5,8 +5,14 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from . import models
 
+from dashboard.models import MemberCard
+from operation.models import WorkoutRecord
+
 
 def report(request):
-    if not request.user.is_authenticated:
+    if not (request.user.is_authenticated and request.user.is_staff):
         return redirect('/')
-    return render(request, 'report/index.html')
+    Member = MemberCard.objects.all()
+    Coach = User.objects.filter(groups__name='coach')
+    Workout = WorkoutRecord.objects.all()
+    return render(request, 'report/index.html', {'member': Member, 'coach': Coach, 'workout': Workout})
