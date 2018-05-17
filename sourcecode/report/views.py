b9df5b0c0f66cd5748c695635d503df6780e6fc8
ecwu <bitlabwzh@gmail.com>
2018-05-17 21:00:00 +0800

    add more report information

--- sourcecode/report/views.py
+++ sourcecode/report/views.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from . import models
 
 from dashboard.models import MemberCard
-from operation.models import WorkoutRecord
+from operation.models import WorkoutRecord, RechargeLog
 
 
 def report(request):
@@ -15,4 +15,12 @@ def report(request):
     Member = MemberCard.objects.all()
     Coach = User.objects.filter(groups__name='coach')
     Workout = WorkoutRecord.objects.all()
-    return render(request, 'report/index.html', {'member': Member, 'coach': Coach, 'workout': Workout})
+    Recharge = RechargeLog.objects.all()
+    Recharge_waiting = RechargeLog.objects.filter(is_valid=False)
+    Recharge_balance = len(Recharge) * 20
+    return render(request, 'report/index.html', {'member': Member,
+                                                 'coach': Coach,
+                                                 'workout': Workout,
+                                                 'recharge': Recharge,
+                                                 'w_recharge': Recharge_waiting,
+                                                 'balance': Recharge_balance})
