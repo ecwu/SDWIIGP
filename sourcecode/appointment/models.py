05e0b456d85f4d4b595139f9d59505a644ea4d7d
ecwu <bitlabwzh@gmail.com>
2018-05-20 17:56:12 +0800

    create model for appointment and add it to admin panel

--- sourcecode/appointment/models.py
+++ sourcecode/appointment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from dashboard.models import MemberCard
 # Create your models here.
+
+
+class Appointment(models.Model):
+    appointer = models.ForeignKey(MemberCard, on_delete=models.DO_NOTHING)
+    appointee = models.ForeignKey(User, limit_choices_to={'groups__name': "coach"}, on_delete=models.DO_NOTHING)
+    appoint_time_date = models.DateField()
+    appoint_time_hour = models.IntegerField()
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-appoint_time_date', 'appoint_time_hour']
+
+    def __str__(self):
+        return str(self.id)
