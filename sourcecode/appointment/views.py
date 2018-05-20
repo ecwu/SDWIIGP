28f5a10f91c0cf6e8bcf1a775d64494810cf4c38
ecwu <bitlabwzh@gmail.com>
2018-05-20 17:56:59 +0800

    create appointment records page

--- sourcecode/appointment/views.py
+++ sourcecode/appointment/views.py
@@ -5,12 +5,22 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 
+from .models import Appointment
+from dashboard.models import MemberCard
+
 
 def exist_appointment(request):
     if not request.user.is_authenticated:
         messages.add_message(request, messages.ERROR, 'Login required.')
         return redirect('/')
-    return render(request, 'appointment/appointment.html')
+    if request.user.is_staff or request.user.is_superuser:
+        records = Appointment.objects.all()
+    elif request.user.groups.filter(name='coach').exists():
+        records = Appointment.objects.filter(appointee=request.user)
+    else:
+        user_membercard = MemberCard.objects.get(related_user=request.user)
+        records = Appointment.objects.filter(appointer=user_membercard)
+    return render(request, 'appointment/appointment.html', {'records': records})
 
 
 def new_appointment(request):
@@ -20,5 +30,8 @@ def new_appointment(request):
     if request.user.groups.filter(name='coach').exists():
         messages.add_message(request, messages.ERROR, 'No Permission.')
         return redirect('/dashboard')
+    if request.user.is_authenticated and request.user.is_staff:
+        messages.add_message(request, messages.WARNING,
+                             'This account have no Member Card . You cannot make appointments.')
     Coach = User.objects.filter(groups__name='coach')
     return render(request, 'appointment/new.html', {'coach': Coach})
